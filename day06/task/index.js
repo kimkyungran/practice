d169a5f2186039df0a319620751c050e9638d57c
kimkyungran <kyungran@nate.com>
2019-12-09 16:52:55 +0900

    day6 task - local.storage , select/value

---
+++ day06/task/index.js
@@ -0,0 +1,24 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+// import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+const select = document.querySelector(".form");
+
+function handleChange() {
+  const selected = select.value;
+  console.log(selected);
+
+  localStorage.setItem("country", selected);
+}
+
+function loadCountries() {
+  const selected = localStorage.getItem("country");
+
+  if (selected) {
+    const option = document.querySelector(`option[value="${selected}"]`);
+
+    option.selected = true;
+  }
+}
+loadCountries();
+select.addEventListener("change", handleChange);
